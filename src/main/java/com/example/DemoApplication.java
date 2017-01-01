384bdec73583770910bcb392bdf1a45d1833e803
Yijun Blue Yuan <bbbush.yuan@gmail.com>
2017-01-01 11:11:08 -0600

    add log configuration
    need to set JDK to default (the one comes with intelliJ has a conflicting slf4j-log4j12)
    
    note the STDOUT configuration override the one from spring-boot

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MarkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
+        log.warn(MarkerFactory.getMarker("APP"), "hello, {} {}", "world", 8);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
