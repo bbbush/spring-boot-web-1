116b81d0ff584b06e5ba5ab72f8075c5f8ad5a74
Yijun Blue Yuan <bbbush.yuan@gmail.com>
2017-01-02 00:08:28 -0600

    follow ref guide to merge main() and SpringBootServletInitializer
    http://docs.spring.io/autorepo/docs/spring-boot/current/reference/htmlsingle/#howto-convert-an-existing-application-to-spring-boot

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,16 +3,29 @@ package com.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MarkerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         log.warn(MarkerFactory.getMarker("APP"), "hello, {} {}", "world", 8);
-		SpringApplication.run(DemoApplication.class, args);
-	}
+        configureApplication(new SpringApplicationBuilder())
+            .run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder appBuilder) {
+        log.warn(MarkerFactory.getMarker("APP"), "in servlet initializer");
+        return configureApplication(appBuilder);
+    }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder appBuilder) {
+        return appBuilder.sources(DemoApplication.class);
+    }
 }
+
